9ee337ca18901751e84af5336b4f67278939607b
praveen3073 <pop3073@gmail.com>
2020-11-15 22:14:42 +0530

    Added: Count Of Contacts In AddressBook Array

--- Contact.js
+++ Contact.js
@@ -202,3 +202,14 @@ deleteContactFromAB("Tanmay", "Jain");
 process.stdout.write(addressBook.join("\n").toString());
 
 console.log("\n---------------------------------");
+// UC6 Get number of contacts in address book
+function countContactsInAB() {
+    function getCount(count, contact) {
+        return count + 1;
+    }
+    return addressBook.reduce(getCount, 0);
+}
+
+process.stdout.write("No of contacts in address book: " + countContactsInAB());
+
+console.log("\n---------------------------------");
